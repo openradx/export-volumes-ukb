c02b1320cb4e4df5d28eaca759ea4a75076188e7
Kai Schlamp <kai.schlamp@gmail.com>
2025-01-29 00:06:44 +0000

    Improve filename sanitization

--- export_volumes_pipeline/utils.py
+++ export_volumes_pipeline/utils.py
@@ -14,9 +14,37 @@ def sanitize_filename(name):
     pattern = r'[<>:"/\\|?*\x00-\x1F]'
     # Replace illegal characters with an underscore
     sanitized_name = re.sub(pattern, "_", name)
-    # Trim leading and trailing whitespace
+    # Trim leading and trailing whitespaces
     sanitized_name = sanitized_name.strip()
-    # Ensure the name is not empty
-    if not sanitized_name:
+    # Trim trailing dots
+    sanitized_name = sanitized_name.rstrip(".")
+    # Replace any remaining whitespace with a dash
+    sanitized_name = re.sub(r"\s+", "-", sanitized_name)
+    # Ensure the name is not empty or a reserved name
+    reserved_names = {
+        "CON",
+        "PRN",
+        "AUX",
+        "NUL",
+        "COM1",
+        "COM2",
+        "COM3",
+        "COM4",
+        "COM5",
+        "COM6",
+        "COM7",
+        "COM8",
+        "COM9",
+        "LPT1",
+        "LPT2",
+        "LPT3",
+        "LPT4",
+        "LPT5",
+        "LPT6",
+        "LPT7",
+        "LPT8",
+        "LPT9",
+    }
+    if not sanitized_name or sanitized_name.upper() in reserved_names:
         sanitized_name = "default_name"
     return sanitized_name
